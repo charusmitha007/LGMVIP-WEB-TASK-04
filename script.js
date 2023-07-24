b1096d8a6d72e5306fcde2806e24d7e9bda23f03
charusmitha007 <108459322+charusmitha007@users.noreply.github.com>
2023-07-24 23:12:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function appendToResult(value){
+    document.getElementById("result").value+=value;
+}
+function clearResult(){
+    document.getElementById("result").value="";
+}
+function calculate(){
+    let result=document.getElementById("result").value;
+    try{
+        document.getElementById("result").value=eval(result);
+    }catch(error){
+        document.getElementById("result").value="Error";
+    }
+}
+function back(){
+    let result=document.getElementById("result").value;
+    document.getElementById("result").value=result.slice(0,-1);
+}
